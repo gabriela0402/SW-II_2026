5153fbb301dae47efae8135c43a35d9d85f45f8a
gabriela0402 <161508311+gabriela0402@users.noreply.github.com>
2026-02-10 17:31:10 -0300

    Add files via upload

---
+++ aula01/index.php
@@ -0,0 +1,15 @@
+<?php
+// Exemplo 1 
+    echo "Olá, Mundo!";
+
+// Exemplo 2
+    $nome ="Gabi";
+    $idade = 17;
+    $preco = 10.50;
+    $ativo = true;
+    
+    echo"Nome: ".$nome . "<br>";
+    echo"Idade: ".$idade . "<br>";
+    echo"Preço: ".$preco . "<br>";
+    echo"Status:".($ativo ? "Ativo":"Inativo");
+?>
